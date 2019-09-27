da486c5ba0ccb4d1fd85aacd22d63dd2f14dbab5
marcgardent <marcgardent@yahoo.fr>
2019-09-27 23:50:30 +0200

    blackbox.py runs DeepEvolve with dummy_train_and_score_callback
    WIP: keras.py

---
+++ Backend/basicia/blackbox.py
@@ -0,0 +1,76 @@
+from DeepEvolve import Evolver
+import logging
+import random;
+# Setup logging.
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO
+)
+
+class DeepEvolveParams:
+    
+    def __init__(self):
+        self.population = 30
+        self.all_possible_genes = {
+            'nb_neurons': [8, 16, 32, 64, 128, 256, 512, 768, 1024],
+            'nb_layers':  [1, 2, 3, 4 ,5],
+            'activation': ['relu', 'elu', 'tanh', 'sigmoid', 'hard_sigmoid','softplus','linear'],
+            'optimizer':  ['rmsprop', 'adam', 'sgd', 'adagrad','adadelta', 'adamax', 'nadam']
+        }
+
+        # replace nb_neurons with 1 unique value for each layer
+        # 6th value reserved for dense layer <--- TODO avoid hardcode!
+        nb_neurons = self.all_possible_genes['nb_neurons']
+        for i in range(1, 7):
+          self.all_possible_genes['nb_neurons_' + str(i)] = nb_neurons
+        # remove old value from dict
+        self.all_possible_genes.pop('nb_neurons')
+
+def dummy_train_and_score_callback(geneparam):
+    """
+    geneparam, for instance:
+        - {'nb_layers': 5, 'activation': 'hard_sigmoid', 'optimizer': 'adamax', 'nb_neurons': [16, 256, 16, 128, 256, 8]}
+    """
+    return random.random()
+
+class BasiciaEvolver:
+    def __init__(self, deepEvolveParams):
+        self.generationIndex =0;
+        self.deepEvolveParams = deepEvolveParams;
+        self.evolver = Evolver(self.deepEvolveParams.all_possible_genes)
+        self.genomes = self.evolver.create_population(self.deepEvolveParams.population)
+
+    def evolve(self):
+        logging.info(f"***Now in generation {self.generationIndex}***")
+        self.print_genomes(self.genomes);
+
+        for genome in self.genomes:
+            genome.train(dummy_train_and_score_callback)
+
+        self.genomes = self.evolver.evolve(self.genomes)
+
+    def evolve_over_time(self,generations):
+        for i in range(0, generations):
+            self.evolve()
+        
+        # Sort our final population according to performance.
+        self.genomes = sorted(self.genomes, key=lambda x: x.accuracy, reverse=True)
+        logging.info(f"***Top 5***")
+        # Print out the top 5 networks/genomes.
+        self.print_genomes(self.genomes[:5])
+
+    def print_genomes(self, genomes):
+        logging.info('-'*80)
+        for genome in genomes:
+            genome.print_genome()
+
+def main():
+    params = DeepEvolveParams();
+    blackbox = BasiciaEvolver(params);
+    blackbox.evolve_over_time(30);
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
---
+++ Backend/basicia/keras.py
@@ -0,0 +1,72 @@
+"""NN Keras Factory"""
+
+from keras.models         import Sequential
+from keras.layers         import Dense, Dropout, Flatten
+from keras.utils.np_utils import to_categorical
+from keras.callbacks      import EarlyStopping, Callback
+from keras.layers         import Conv2D, MaxPooling2D
+from keras                import backend as K
+
+
+def compile_from_genome(genome, input_shape, output_shape):
+
+    actor = Sequential()
+    actor.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+    actor.add(Dense(16))
+    actor.add(Activation('relu'))
+    actor.add(Dense(16))
+    actor.add(Activation('relu'))
+    actor.add(Dense(16))
+    actor.add(Activation('relu'))
+    actor.add(Dense(nb_actions))
+    actor.add(Activation('linear'))
+    
+
+
+def compile_model_cnn(genome, nb_classes, input_shape):
+    """Compile a sequential model.
+
+    Args:
+        genome (dict): the parameters of the genome
+
+    Returns:
+        a compiled network.
+
+    """
+    # Get our network parameters.
+    nb_layers  = genome.geneparam['nb_layers' ]
+    nb_neurons = genome.nb_neurons()
+    activation = genome.geneparam['activation']
+    optimizer  = genome.geneparam['optimizer' ]
+
+    logging.info("Architecture:%s,%s,%s,%d" % (str(nb_neurons), activation, optimizer, nb_layers))
+
+    model = Sequential()
+
+    # Add each layer.
+    for i in range(0,nb_layers):
+        # Need input shape for first layer.
+        if i == 0:
+            model.add(Conv2D(nb_neurons[i], kernel_size = (3, 3), activation = activation, padding='same', input_shape = input_shape))
+        else:
+            model.add(Conv2D(nb_neurons[i], kernel_size = (3, 3), activation = activation))
+        
+        if i < 2: #otherwise we hit zero
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+        
+        model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    # always use last nb_neurons value for dense layer
+    model.add(Dense(nb_neurons[len(nb_neurons) - 1], activation = activation))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes, activation = 'softmax'))
+
+    #BAYESIAN CONVOLUTIONAL NEURAL NETWORKS WITH BERNOULLI APPROXIMATE VARIATIONAL INFERENCE
+    #need to read this paper
+
+    model.compile(loss='categorical_crossentropy',
+              optimizer=optimizer,
+              metrics=['accuracy'])
+
+    return model
